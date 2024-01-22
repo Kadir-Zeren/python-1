76a53e2ae9a2e0436b297867a54eb457faf15e77
kadir-zeren <k4dzeren@gmail.com>
2024-01-22 08:32:00 +0300

    comment

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,3 +5,32 @@ print('''Hello World!''')
 print("I'm great")
 print('"hi" he said')
 print('''"Don't do it" said the manager''')
+
+print('''bu birinci sat
+bu ikinci satır
+bu üçüncü satır''')
+
+
+print('3.14')
+print(3.14)
+
+
+print('3.14'*3)
+print(3.14*3)
+
+print("bir")
+print()
+print("üç")
+print()
+print('') 
+print("") 
+
+print('I\'m great')
+print("\"don't do it\" said the manager")
+print("bu benim cumlem  \nbu ikinci cümlem")
+print("bu benim cumlem \tbu ikinci cümlem")
+
+print("asdasdasdasd asdasd asd asd asdas dsadas  \
+      sadasdasdasdasdasdasdas das as \
+      dsfsfdsfsdfs") # yorum satırı
+print("kullanici ismi") # kullanici ismi alınıdı
