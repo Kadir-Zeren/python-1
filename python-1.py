b448c297fafb37e288e539d109675b85d19da6cd
kadir-zeren <k4dzeren@gmail.com>
2024-09-29 03:15:06 +0300

    python-ended

--- python-1.py
+++ python-1.py
@@ -15,3 +15,56 @@ print("I'm great")
 print ('"hi" he said')
 print('"hi" he said')
 print('''Don't do it" said the manager''')
+print('''Lorem ipsum dolor sit amet,
+fusce ut quisque neque donec,
+massa metus amet, luctus inceptos.''')
+print("""Praesent a, mi mattis velit metus,
+accumsan adipiscing ipsum sit justo
+penatibus, amet mauris non tempus justo.""")
+print('ben bunu yazmak istiyorum')
+print('''bu birinci satir
+bu ikinci satir
+bu uc
+bu dort''')
+print('''bu birinci 'satir
+bu ikinci satir"
+bu uc
+bu dort''')
+print('3.14')
+print(3.14)
+print('3.14'*3)
+print(3.14*3)
+print (567)
+print(''''We should have enough time for our family"''')
+print('first line')
+print()
+print('''third line''')
+print('bir')
+print()
+print('uc')
+print()
+print('')
+print('I\'m great')
+print("\"don't do it\" said the manager")
+print('bu benim cumlem \nbu ikinci satir')
+print('bu benim cumlem \tbu ikinci satir')
+print('being a good person')
+print("ben kodlarkjlj v1 k k k kk 11 lkjlkjlkjljlk\
+kdkdkdkdkdkd\
+kdkdkdkdkd")
+# buradaki fonksiyon kare alma fonksiyonu
+print('kullanici ismi') #Burada kullanici ismi yazddiriliyor.
+x = 5 # burada x degeri atandi
+x = 10
+y = 5
+a = (x / y) * 25
+print(a** 2)
+x = 10
+y = 5
+a = (x/y)*100
+print(a)
+print('hello') # This is an inline comment
+print('hello')
+# First multi-line comment
+# Second multi-line comment
+# Third multi-line comment
